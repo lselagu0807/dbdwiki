1be3f3f294e66b2cb92b0acd6c51f31a63b20375
lselagu0807 <lselagu0807@g.educaand.es>
2026-02-10 14:49:48 +0100

    Servicios

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -6,15 +6,49 @@ import { Service } from '../../services/service';
   templateUrl: './home.html'
 })
 export class Home {
-  personajes: any[] = [];
+  survivors: any[] = [];
+  killers: any[] = [];
+  perks_survis: any[] = [];
+  perks_killer: any[] = [];
+  addons: any[] = [];
 
   constructor(private dbdService: Service) {}
 
   ngOnInit() {
-    this.dbdService.getPersonajes().subscribe({
+    this.dbdService.getSurvivors().subscribe({
       next: (res) => {
-        this.personajes = res;
-        console.log('Personajes cargados:', this.personajes);
+        this.survivors = res;
+        console.log('survivors cargados:', this.survivors);
+      },
+      error: (err) => console.log('F en el chat:', err)
+    });
+
+    this.dbdService.getKillers().subscribe({
+      next: (res) => {
+        console.log('killers cargados: ', this.killers);
+      }
+    })
+
+    this.dbdService.getSurvivorPerksByCode("dwightfairfield").subscribe({
+      next: (res) => {
+        this.perks_survis = res;
+        console.log('perks survis cargadas:', this.perks_survis);
+      },
+      error: (err) => console.log('F en el chat:', err)
+    });
+
+    this.dbdService.getKillerPerksByCode("thetrapper").subscribe({
+      next: (res) => {
+        this.perks_killer = res;
+        console.log('perks killers cargadas:', this.perks_killer);
+      },
+      error: (err) => console.log('F en el chat:', err)
+    });
+
+    this.dbdService.getAddonsByItemCode("flashlight").subscribe({
+      next: (res) => {
+        this.addons = res;
+        console.log('addons cargados:', this.addons);
       },
       error: (err) => console.log('F en el chat:', err)
     });
--- src/app/services/service.ts
+++ src/app/services/service.ts
@@ -1,15 +1,59 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
+
 @Injectable({
   providedIn: 'root',
 })
 export class Service {
-  private url = "https://cyberluis.com/dba/api/survivor";
+  private url = "https://cyberluis.com/dba/api";
+
+  private survivors = [];
 
   constructor(private http: HttpClient) { }
 
-  getPersonajes(): Observable<any[]> {
-    return this.http.get<any[]>(this.url);
+  getSurvivors(): Observable<any[]> {
+    return this.http.get<any[]>(this.url + "/survivor");
   }
+
+  getSurvivorPerksByCode(code: string): Observable<any[]> {
+    // Usamos la ruta que aparece en tu captura para ir directo al grano
+    return this.http.get<any>(`${this.url}/survivor/${code}/perk`).pipe(
+      map(res => {
+        // Según tu captura, los datos vienen dentro de 'data'
+        return res.data || [];
+      })
+    );
+  }
+
+
+  getKillers(): Observable<any[]> {
+    return this.http.get<any[]>(this.url + "/killer");
+  }
+
+  getKillerPerksByCode(code: string): Observable<any[]> {
+    // Usamos la ruta que aparece en tu captura para ir directo al grano
+    return this.http.get<any>(`${this.url}/killer/${code}/perk`).pipe(
+      map(res => {
+        // Según tu captura, los datos vienen dentro de 'data'
+        return res.data || [];
+      })
+    );
+  }
+
+  getItems(): Observable<any[]> {
+    return this.http.get<any[]>(this.url + "/item");
+  }
+
+  getAddonsByItemCode(itemCode: string): Observable<any[]> {
+    // Siguiendo la estructura de tu API: /item/{code}/addon
+    return this.http.get<any>(`${this.url}/item/${itemCode}/addon`).pipe(
+      map(res => {
+        // Accedemos a 'data' que es donde viene el array de addons en tus JSON
+        // Si no hay datos, devolvemos un array vacío para que no pete el *ngFor
+        return res.data || [];
+      })
+    );
+  }
+
 }
